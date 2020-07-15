fb28ea22e92f2c8e8fea64a70932652dbc38b34e
Stella Xu <stellaxu3116@gmail.com>
2020-07-15 16:23:03 -0400

    I added validation logic in the setter methods

--- date.go
+++ date.go
@@ -1,20 +1,35 @@
 package calender
 
+import "errors"
+
 type Date struct {
 	year, month, day int
 }
 
 //the setter methods:
-func (d *Date) SetYear(y int) {
+func (d *Date) SetYear(y int) error {
+	if y < 2000 || y > 3000 {
+		return errors.New("Invalid year")
+	}
 	d.year = y
+	return nil
 	//or
 	//(*d).year = y
 }
-func (d *Date) SetMonth(m int) {
+func (d *Date) SetMonth(m int) error {
+	if m < 1 || m > 12 {
+		return errors.New("Invalid month")
+	}
 	d.month = m
+	return nil
 }
-func (d *Date) SetDay(day int) {
+
+func (d *Date) SetDay(day int) error {
+	if day < 1 || day > 31 {
+		return errors.New("Invalid day")
+	}
 	d.day = day
+	return nil
 }
 
 //the getter methods:
